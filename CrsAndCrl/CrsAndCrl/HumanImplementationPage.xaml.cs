de2b2c522d152f9f66505439c8d9a12810620ccf
Opilane TTHK <opilane@tthk.ee>
2020-09-24 10:51:59 +0300

    Add project files.

---
+++ CrsAndCrl/CrsAndCrl/HumanImplementationPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CrsAndCrl
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HumanImplementationPage : ContentPage
+    {
+
+        bool CrossOrCircle = true;
+
+        public HumanImplementationPage()
+        {
+            InitializeComponent();
+
+            var tapGestureRecognizer = new TapGestureRecognizer();
+            tapGestureRecognizer.Tapped += (s, e) => {
+                Image boxi = (Image)s;
+                if (boxi.Source.ToString().Equals("File: empty.png"))
+                {
+                    if (CrossOrCircle)
+                    {
+                        boxi.Source = "cross.png";
+                    }
+                    else
+                    {
+                        boxi.Source = "circle.png";
+                    }
+                }
+                CrossOrCircle = !CrossOrCircle;
+            };
+
+            for (int y = 0; y < 3; y++)
+            {
+                for (int x = 0; x < 3; x++)
+                {
+                    Image box = new Image {
+                        BackgroundColor = Color.White,
+                        Opacity = 0.7,
+                        Margin = 2,
+                        Source = "empty.png"
+                    };
+                    box.GestureRecognizers.Add(tapGestureRecognizer);
+                    grid.Children.Add(box, y, x);
+                }
+            }
+        }
+    }
+}
---
+++ CrsAndCrl/CrsAndCrl/MainMenu.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CrsAndCrl
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainMenu : ContentPage
+    {
+        public MainMenu()
+        {
+            InitializeComponent();
+
+            NavigateCommand = new Command<Type>(async (Type pageType) =>
+            {
+                Page page = (Page)Activator.CreateInstance(pageType);
+                await Navigation.PushAsync(page);
+            });
+
+            BindingContext = this;
+        }
+
+        public ICommand NavigateCommand { private set; get; }
+
+        private async void HumanButton_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new HumanImplementationPage());
+        }
+
+        private async void ComputerButton_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ComputerImplementationPage());
+        }
+    }
+}
